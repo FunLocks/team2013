deba1e91f9fd50e5d7c95a2395d897d90da68efd
Tomoya-Kanazashi <75833884+Tomoya-Kanazashi@users.noreply.github.com>
2020-12-18 22:10:05 +0900

    Point とRankingを更新した

--- Covid-19 Avoider Ranking/app/src/main/java/com/example/covid_19avoiderranking/Point.java
+++ Covid-19 Avoider Ranking/app/src/main/java/com/example/covid_19avoiderranking/Point.java
@@ -1,53 +1,138 @@
 package com.example.covid_19avoiderranking;
 
-import java.util.Calendar;
+import android.util.Log;
 
-public class Ranking {
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Point {
 
    private  int point = 0;
-   private Strng user;
-    int hour, minute, second, week;
+   private String user;
+    private static final ThreadPoolExecutor EXECUTOR = new ThreadPoolExecutor(2, 4,
+            60, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
 
+
+   private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+   private Calendar calendar = new Calendar() {
+       @Override
+       protected void computeTime() {
+
+       }
+
+       @Override
+       protected void computeFields() {
+
+       }
+
+       @Override
+       public void add(int field, int amount) {
+
+       }
+
+       @Override
+       public void roll(int field, boolean up) {
+
+       }
+
+       @Override
+       public int getMinimum(int field) {
+           return 0;
+       }
+
+       @Override
+       public int getMaximum(int field) {
+           return 0;
+       }
+
+       @Override
+       public int getGreatestMinimum(int field) {
+           return 0;
+       }
+
+       @Override
+       public int getLeastMaximum(int field) {
+           return 0;
+       }
+   };
+
+    public void setup() {//データベースを使えるようにしている。
+        // [START get_firestore_instance]
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // [END get_firestore_instance]
+
+        // [START set_firestore_settings]
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
+        // [END set_firestore_settings]
+    }//データベースを使えるようにしている。
+
+    public void setupCacheSize() {
+        // [START fs_setup_cache]
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setCacheSizeBytes(FirebaseFirestoreSettings.CACHE_SIZE_UNLIMITED)
+                .build();
+        db.setFirestoreSettings(settings);
+        // [END fs_setup_cache]
+    }
+    private int hour, minute, second, week;
+    private static final String TAG = "Point";
+    private int i=0;
     public int getPoint(){
         return point;
     }
 public String getUser(){
         return user;
 }
-
-    if(i => 10){
-point = point - 1;
+public void timeCalendar() {
+    if (i >=10){
+        point = point - 1;
     }else{
         point = point + 1;
     }
 
-    hour = calender.get(Calendar.HOUR_OF_DAY);
+    hour = calendar.get(Calendar.HOUR_OF_DAY);
     minute = calendar.get(Calendar.MINUTE);
     second = calendar.get(Calendar.SECOND);
     week = calendar.get(Calendar.DAY_OF_WEEK);
- if(hour == 0 && minute == 0 && second == 0 && week == 2){
-     point = 0;
+    if (hour == 0 && minute == 0 && second == 0 && week == 2) {
+        point = 0;
     }
-
+}
+public void user_pointadd() {
     Map<String, Object> pointData = new HashMap<>();
- pointData.put("user_point", "point");
- pointData.put("name", "user");
+    pointData.put("user_point", "point");
+    pointData.put("name", "user");
 
- db.collection("pointdb").document("ppoint")
-         .add(pointData)
-        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-        @Override
-        public void onSuccess(DocumentReference documentReference) {
-            Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-        }
-    })
+    db.collection("pointdb")//.document("ppoint")
+            .add(pointData)
+            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                }
+            })
             .addOnFailureListener(new OnFailureListener() {
         @Override
         public void onFailure(@NonNull Exception e) {
             Log.w(TAG, "Error adding document", e);
         }
     });
-
+}
 //ポイントのアップデート
  /*public void updateDocument() {
         DocumentReference washingtonRef = db.collection("pointdb").document("point");
@@ -68,5 +153,5 @@ point = point - 1;
                 });
     }*/
 
-    return point;
+    //return point;
 }
--- Covid-19 Avoider Ranking/app/src/main/java/com/example/covid_19avoiderranking/Ranking.java
+++ Covid-19 Avoider Ranking/app/src/main/java/com/example/covid_19avoiderranking/Ranking.java
@@ -1,25 +1,39 @@
 package com.example.covid_19avoiderranking;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class Ranking {
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = "Ranking";
+    public void Sort() {
+        CollectionReference PRef = db.collection("pointdb");
+        PRef.orderBy("user_point").limit(10);
+        String fieldname = "user_point";
+        db.collection("pointdb")
+                .whereEqualTo(fieldname,PRef). limit(10)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
 
-    DocumentReference PRef = db.collection("pointdb").document("ppoint")
-    PRef.orderBy("user_point").limit(10);
 
-db.collection("pointdb")
-        .whereEqualTo("user_points", limit(10))
-        .get()
-        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-        @Override
-        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-            if (task.isSuccessful()) {
-                for (QueryDocumentSnapshot document : task.getResult()) {
-                    Log.d(TAG, document.getId() + " => " + document.getData());
-                }
-            } else {
-                Log.d(TAG, "Error getting documents: ", task.getException());
-            }
-        }
-    });
-
-
+    }
 }
